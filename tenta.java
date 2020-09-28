27086c0b6b353bde7486c72df3cdf3b359daf508
Sinjebos <Nicke_95_@hotmail.com>
2020-09-28 14:15:56 +0200

    added methods and question

--- tenta.java
+++ tenta.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class tenta {
     static Scanner sc = new Scanner(System.in);
 
-    public static void tentaquestion(){
+    public static void options1(){
         System.out.println("Which of these are correct: ");
-        System.out.println("[1] for(int i = 0 i < 10; i++");
+        System.out.println("[1] for(int i = 0 i < 10; i++)");
         System.out.println("[2] for int i; i < 10; i++");
-        System.out.println("[3] for (int i = 0; i < 10; i++");
-        System.out.println("[4] for (int i = 0; i < 10; i+");
-
+        System.out.println("[3] for(int i = 0; i < 10; i++)");
+        System.out.println("[4] for (int i = 0; i < 10; i+)");
+        System.out.println("[5] skip to next question");
+    }
+    public static void options2(){
+        System.out.println("Which of these are correct: ");
+        System.out.println("[1] ArrayList String x = new ArrayList(); ");
+        System.out.println("[2] ArrayList<String> x = new ArrayList<>(); ");
+        System.out.println("[3] ArrayList String = new ArrayList<>(); ");
+        System.out.println("[4] ArrayList String x = new Arraylist<>;");
+    }
+    public static void questions(){
+        options1();
         switch(sc.next()){
             case "1":
             System.out.println("Sadly wrong answer");
@@ -23,12 +33,33 @@ public class tenta {
             case "4":
             System.out.println("noo wrong answer");
             break;
+            case "5":
+            options2();
+            switch(sc.next()){
+                case "1":
+                System.out.println("oof so close but wrong! ");
+                break;
+                case "2":
+                System.out.println("Yay good job! nailed it!");
+                break;
+                case "3":
+                System.out.println("Wrong answer!");
+                break;
+                case "4": 
+                System.out.println("nono wrong answer");
+                break;
+                default:
+                System.out.println("input 1");
+                break;
+            }
+            break;
+
             default:
             System.out.println("invalid answer, input 1-4");
             break;
         }
     }
     public static void main(String[] args) {
-        tentaquestion();
+        questions();
     }
 }
